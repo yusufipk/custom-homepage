1bc87f520c4a9e4942611172815e4fc9797eb81a
Yusuf Ipek <yusufipek@protonmail.com>
2021-04-14 05:20:17 +0300

    add process.env and state only changes when response is valid

--- src/components/fetch weather/weather-fetch.jsx
+++ src/components/fetch weather/weather-fetch.jsx
@@ -5,25 +5,44 @@ class Forecast extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      weather: null,
+      weather: {
+        response: 1,
+        weather: [
+          {
+            icon: "04n",
+          },
+        ],
+        main: {
+          temp: 285.66,
+        },
+      },
     };
   }
 
   async componentDidMount() {
-    const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=yourapikey"
-    );
-    const json = await response.json();
-    this.setState({ weather: json });
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=${process.env.REACT_APP_API_KEY}`
+      );
+      const json = await response.json();
+      if (json.cod !== 401) {
+        this.setState({ weather: json });
+      }
+
+      this.setState({ response: 1 });
+    } catch (err) {
+      console.log("Error:", err);
+    }
   }
 
   render() {
-    if (!this.state.weather) {
-      return <div />;
-    }
     function kelvinToCelsius(kel) {
       let celsius = kel - 273.15;
       return celsius.toFixed(2);
+    }
+
+    if (this.state.weather.response === 1) {
+      return <div />;
     }
 
     return (
@@ -32,7 +51,8 @@ class Forecast extends Component {
           src={`https://openweathermap.org/img/wn/${this.state.weather.weather[0].icon}@2x.png`}
           alt=""
         />
-        İstanbul Hava Durumu: {kelvinToCelsius(this.state.weather.main.temp)}°С
+        İstanbul Hava Durumu: {kelvinToCelsius(this.state.weather.main.temp)}
+        °С
       </div>
     );
   }
